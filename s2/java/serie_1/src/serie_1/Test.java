2c8486fc73cc28c3db8180a57f80cb4ec765bd8c
Bartholomé Gili <bartholome.gili@etu.univ-lyon1.fr>
2021-03-05 11:17:43 +0100

    java/serie_1 fixed for returning

--- s2/java/serie_1/src/serie_1/Test.java
+++ s2/java/serie_1/src/serie_1/Test.java
@@ -16,10 +16,23 @@ public class Test {
      */
     public static void main(String[] args) {
         
-        int n = Integer.parseInt(args[0]);
-        System.out.println(n + (Carré.carréParfait(n) ? " est un carré parfait": " n'est pas un carré parfait"));
+        try {
+            
+            int n = Integer.parseInt(args[0]);
+            System.out.println(n + (Carré.carréParfait(n) ? " est un carré parfait": " n'est pas un carré parfait"));
+            System.out.println("\n\n==========================\n\nAprès " + 19 + " années, votre capital vaudra : " + CaisseEpargne.calculCapital(19));
+
+        } catch (NumberFormatException e) {
+            
+            System.out.println("L'argument n'est pas un entier");
+            
+        } catch ( Exception e) {
+            
+            System.out.println("Une erreur est survenue : " + e);
+        }
+
         
-        System.out.println("Après " + 19 + " années, votre capital vaudra : " + CaisseEpargne.calculCapital(19));
+        
     }
     
 }
